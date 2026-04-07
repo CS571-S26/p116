734ec1bb9141992da5097ef57b293bc9f1bd0502
Kanuj Verma <108971272+KanujVerma@users.noreply.github.com>
2026-04-07 17:18:32 -0500

    feat: replace custom Navbar with Bootstrap Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,25 @@
 import { NavLink } from 'react-router-dom'
+import { Navbar as BsNavbar, Nav, Container } from 'react-bootstrap'
 import './Navbar.css'
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="navbar__inner">
-        <NavLink to="/" className="navbar__logo">MIMS</NavLink>
-        <ul className="navbar__links">
-          <li><NavLink to="/" end>Home</NavLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
-          <li><NavLink to="/leadership">Leadership</NavLink></li>
-          <li><NavLink to="/events">Events</NavLink></li>
-          <li><NavLink to="/my-events">My Events</NavLink></li>
-        </ul>
-      </div>
-    </nav>
+    <BsNavbar className="mims-navbar" expand="md" sticky="top">
+      <Container fluid className="mims-navbar__inner">
+        <BsNavbar.Brand as={NavLink} to="/" className="mims-navbar__logo">
+          MIMS
+        </BsNavbar.Brand>
+        <BsNavbar.Toggle aria-controls="main-nav" />
+        <BsNavbar.Collapse id="main-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+            <Nav.Link as={NavLink} to="/leadership">Leadership</Nav.Link>
+            <Nav.Link as={NavLink} to="/events">Events</Nav.Link>
+            <Nav.Link as={NavLink} to="/my-events">My Events</Nav.Link>
+          </Nav>
+        </BsNavbar.Collapse>
+      </Container>
+    </BsNavbar>
   )
 }
